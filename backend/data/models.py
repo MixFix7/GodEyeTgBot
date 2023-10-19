e5b89a23701ba8ebd0f3aa1cc3bbf5e4726fd9e6
MixFix7 <mashtalar95@gmail.com>
2023-10-19 21:15:53 +0300

    made models of person

--- backend/data/models.py
+++ backend/data/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from phonenumber_field.modelfields import PhoneNumberField
+from django.contrib.postgres.fields import ArrayField
 
-# Create your models here.
+
+class PersonData(models.Model):
+    name = models.CharField(max_length=256, null=True, blank=True)
+    phone_number = PhoneNumberField(null=True, blank=True)
+    ip = models.GenericIPAddressField(null=True, blank=True)
+    organization = models.CharField(max_length=256, null=True, blank=True)
+    location = models.CharField(max_length=500, null=True, blank=True)
+    city = models.CharField(max_length=500, null=True, blank=True)
+    emails = ArrayField(models.CharField(max_length=256), null=True, blank=True)
+
+
+class SocialData(models.Model):
+    person = models.ForeignKey(PersonData, related_name='social_of_person', on_delete=models.CASCADE)
+
+    instagram = models.CharField(max_length=256, null=True, blank=True)
+    facebook = models.CharField(max_length=256, null=True, blank=True)
+    tiktok = models.CharField(max_length=256, null=True, blank=True)
+    paypal = models.CharField(null=True, blank=True)
+    twitter = models.URLField(max_length=256, null=True, blank=True)
+    snapchat = models.CharField(max_length=256, null=True, blank=True)
