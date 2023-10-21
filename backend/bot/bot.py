a4b4a77062fdbeb7fcbe1d4c4e6f89f145f2ed6a
MixFix7 <mashtalar95@gmail.com>
2023-10-21 23:12:28 +0300

    created commands for searching in the bot

--- backend/bot/bot.py
+++ backend/bot/bot.py
@@ -1,6 +1,7 @@
 from telebot.async_telebot import AsyncTeleBot
 from django.conf import settings
 from . import messages
+import requests
 
 
 bot = AsyncTeleBot(settings.BOT_TOKEN, parse_mode='HTML')
@@ -15,10 +16,56 @@ async def main():
     await bot.set_webhook(url=webhook_url)
 
 
-"""COMMAND HANDLERS"""
+""" COMMAND HANDLERS """
 
 
 @bot.message_handler(commands=['start'])
 async def start(message):
     await bot.send_message(message.chat.id, messages.start_message)
+
+
+@bot.message_handler(commands=['find_person'])
+async def handler_find_person(message):
+    await bot.send_message(message.chat.id, messages.find_person_message)
+    state[message.from_user.id] = 'finding_person'
+
+
+""" COMMANDS FUNCTIONS """
+
+
+@bot.message_handler(func=lambda message: state.get(message.chat.id) is None)
+async def unknown_command(message):
+    await bot.send_message(message.chat.id, messages.unknown_command_message)
+
+
+@bot.message_handler(func=lambda message: state.get(message.from_user.id) == 'finding_person')
+async def find_person(message):
+    text_lines = message.text.split('\n')
+    data = {}
+    for line in text_lines:
+        key, value = line.split(' - ')
+        data[key] = value
+
+    response = requests.post(f"{server_url}/data/find-person/", data=data)
+    persons_data = response.json()
+
+    if persons_data:
+        for person in persons_data:
+            person_data = f"*** DATA OF {person['name']} ***"
+            for field, value in person.items():
+                if field != 'social_data':
+                    person_data += f"\n{field}: {value}"
+            else:
+                person_data += "\n"
+            if 'social_data' in person and person['social_data']:
+                social_data = f"\n*** SOCIAL DATA OF {person['name']} ***"
+                for social_field, social_value in person['social_data'][0].items():
+                    social_data += f"\n{social_field}: {social_value}"
+                person_data += social_data
+            await bot.send_message(message.chat.id, person_data)
+
+    else:
+        await bot.send_message(message.chat.id, messages.nothing_found_message)
+
+
 
--- backend/bot/messages.py
+++ backend/bot/messages.py
@@ -2,3 +2,23 @@ start_message = """
 <b>Welcome, i am the bot, what have a database of all people on our planet</b>
 <b>Write the /search command to find information about person</b>
 """
+
+find_person_message = """
+Ok, send me a form of the person, who you wanna find
+
+<b>Example form:</b>
+
+name - Jax Rubber
+phone_number - +000000000
+city - USA, Washington
+
+"""
+
+unknown_command_message = """
+    <b>Unknown command, /help to show all commands </b>
+"""
+
+
+nothing_found_message = """
+    <b>Sorry, nothing not found, please try again</b>
+"""
